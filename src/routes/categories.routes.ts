1a297714ce9b72f0638b17b8a67f068e0e867b47
silva4dev <web4dev.lucas@outlook.com>
2023-06-22 16:25:16 -0300

    chore: change categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
-import { importCategoryController } from "@modules/cars/useCases/importCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
 
